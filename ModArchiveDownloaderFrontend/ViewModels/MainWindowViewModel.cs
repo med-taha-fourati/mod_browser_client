80a6d506616036f7f699d213c168bd8d9d8009e6
med-taha-fourati <tahafourati11edu@gmail.com>
2025-08-12 00:58:49 +0100

    todo: fix height issue

--- ModArchiveDownloaderFrontend/ViewModels/MainWindowViewModel.cs
+++ ModArchiveDownloaderFrontend/ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ModArchiveDownloaderFrontend.Models;
 using ModArchiveDownloaderFrontend.Repository;
+using Enumerable = System.Linq.Enumerable;
 
 namespace ModArchiveDownloaderFrontend.ViewModels;
 
@@ -21,15 +22,24 @@ public partial class MainWindowViewModel : ViewModelBase
     private string _searchText = string.Empty;
 
     [ObservableProperty] 
-    private ObservableCollection<Module> _items = new();
+    private Module[] _items = new Module[0];
+
+    [ObservableProperty] private string _elements = "Modules found: 0";
     public string ErrorMessage { get; } = "Unable to find";
     public string SearchBeginning { get; } = "Get Started by searching at the top left";
-    public bool HasNoItems => Items.Count == 0;
+    public bool HasNoItems => Items.Length == 0;
+    
+    partial void OnItemsChanged(Module[] value)
+    {
+        OnPropertyChanged(nameof(HasNoItems));
+        OnPropertyChanged(nameof(Elements));
+    }
 
     private async Task FetchModules()
     {
         var modules = await new ApiRepository().GetModules(SearchText ?? string.Empty);
 
-        Items = modules;
+        Items = Enumerable.ToArray(modules);
+        Elements = "Modules found: " + Items.Length;
     }
 }
