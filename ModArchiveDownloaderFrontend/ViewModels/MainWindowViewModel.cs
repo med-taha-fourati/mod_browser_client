ce9d5aa86a1456dcda147fad8e43425774be2495
med-taha-fourati <tahafourati11edu@gmail.com>
2025-08-11 11:33:16 +0100

    more frontend edits

--- ModArchiveDownloaderFrontend/ViewModels/MainWindowViewModel.cs
+++ ModArchiveDownloaderFrontend/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace ModArchiveDownloaderFrontend.ViewModels;
 
-public partial class MainWindowViewModel : ViewModelBase
+public class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
     public Module[] Items { get; set; } = new[]
@@ -20,4 +20,8 @@ public partial class MainWindowViewModel : ViewModelBase
         new Module { FileName = "test.txt", ModuleCode = "test" },
         new Module { FileName = "thunder_the_secret.symmod", ModuleCode = "thunder_the_secret" }
     };
+    public string ErrorMessage { get; } = "Unable to find";
+    public string SearchBeginning { get; } = "Get Started by searching at the top left";
+    
+    public bool HasNoItems => Items.Length == 0 || Items == null;
 }
